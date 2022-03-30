20675f7741510cd4c45aa9b38278197cb368eb02
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-29 17:25:36 -0700

    UNION_V1.h
    An archive of the first version of the union file for this directory set.

---
+++ Codeviet_Union/!OldVersions/UNION/1/1-100/UNION_V1.h
@@ -0,0 +1,17 @@
+// Start of script
+// Codeviet union header file
+// Original name: C-Viet Union
+// The Union type is the core of this file
+union codeviet { // Parody of the Soviet Union
+ printf("Welcome to the Codeviet Union!");
+ break;
+}
+return codeviet();
+return 0;
+exit;
+/* File info
+* File type: C Header file (*.h)
+* File version: 1 (2022, Tuesday, March 29th at 5:25 pm)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
